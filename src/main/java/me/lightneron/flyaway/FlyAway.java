dbb091cb99fec85e3c2ef7e1f1c5d47067308427
BuildTools <unconfigured@null.spigotmc.org>
2024-02-21 21:01:02 +0300

    None

---
+++ src/main/java/me/lightneron/flyaway/FlyAway.java
@@ -0,0 +1,17 @@
+package me.lightneron.flyaway;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FlyAway extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
