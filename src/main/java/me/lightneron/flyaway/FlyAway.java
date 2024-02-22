8c88a33b4b026abd5372c0c3c2a2b16a417792b5
LightNeron <alex_miner2021@mail.ru>
2024-02-22 14:52:33 +0300

    Главный класс

--- src/main/java/me/lightneron/flyaway/FlyAway.java
+++ src/main/java/me/lightneron/flyaway/FlyAway.java
@@ -1,13 +1,23 @@
 package me.lightneron.flyaway;
 
+import me.lightneron.flyaway.Commands.FlyCommand;
+import me.lightneron.flyaway.Listeners.ElytraListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class FlyAway extends JavaPlugin {
 
+
+
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        if (!getDataFolder().exists()) {
+            getDataFolder().mkdirs();
+        }
+        saveDefaultConfig();
+        FlyCommand flyCommand = new FlyCommand();
+        flyCommand.fly();
+        getServer().getPluginManager().registerEvents(new ElytraListener(), this);
     }
 
     @Override
